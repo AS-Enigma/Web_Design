b323dab28f5915963de857558f25f2e4a50cb07d
AS-Enigma <149681032+AS-Enigma@users.noreply.github.com>
2024-04-12 14:15:50 -0400

    JS Practice Finished

---
+++ JS_Practice/JS_Practice1/script.js
@@ -0,0 +1,13 @@
+let myVar = 5;
+console.log(myVar);
+
+myVar += 5;
+console.log(myVar);
+
+const add_this = 2;
+
+function incrementValue(inital, incrementor) {
+    return inital + incrementor;
+}
+
+console.log(incrementValue(myVar, add_this));
